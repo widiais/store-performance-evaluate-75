1779b4a6fcd3489880cb451815e308931b23469e
widiais <widiahmadibnu@gmail.com>
2025-02-09 19:07:16 +0700

    Add routing for ESP report pages
    Configure routing for ESP report and detail pages in the main App component, enabling navigation to the newly implemented ESP report views.

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "@/pages/Index";
 import ChampReport from "@/components/ChampReport";
 import ChampReportDetail from "@/components/ChampReportDetail";
+import EspReport from "@/components/EspReport";
+import EspReportDetail from "@/components/EspReportDetail";
 import { ThemeProvider } from "./components/ui/theme-provider";
+
 
 const queryClient = new QueryClient();
 
@@ -21,6 +24,8 @@ const App = () => (
             <Route path="/" element={<Index />} />
             <Route path="/report" element={<ChampReport />} />
             <Route path="/report/:id" element={<ChampReportDetail />} />
+            <Route path="/esp-report" element={<EspReport />} />
+            <Route path="/esp-report/:id" element={<EspReportDetail />} />
           </Routes>
         </Router>
       </ThemeProvider>
